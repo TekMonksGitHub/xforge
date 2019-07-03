c3f1955caa6d68cd2d3a8880b6ebba9e249d6b57
Rohit Kapoor <rvkapoor@Rohits-MacBook.local>
2019-07-02 23:08:00 -0400

    Cleanup

--- install.bat
+++ install.bat
@@ -1,4 +1,3 @@
 @echo off
-npm install commander
 npm install shelljs
 npm install colors
--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-npm install commander
-npm install shelljs
-npm install colors
+npm install shelljs -g
+npm install colors -g
