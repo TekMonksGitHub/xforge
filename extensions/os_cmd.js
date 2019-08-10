c92efb1821cae8c8074a0c27349ce59d1143d9e9
Rohit Kapoor <rvkapoor@Rohits-MacBook.lan>
2019-08-11 01:51:50 +0800

    - ProC compiler - Fixes - Streaming for OS cmds.

--- extensions/os_cmd.js
+++ extensions/os_cmd.js
@@ -7,7 +7,7 @@
 const {exec} = require("child_process");
 const {Ticketing} = require(`${CONSTANTS.LIBDIR}/Ticketing.js`);
 
-exports.os_cmd = cmd => {
+exports.os_cmd = (cmd, stream = false) => {
     if (!CONSTANTS.OBJECT_STORE["ext.os_cmd.ticketing"])
         CONSTANTS.OBJECT_STORE["ext.os_cmd.ticketing"] = new Ticketing(CONSTANTS.MAX_PROCESSES, "Process pool exhaused, waiting.");
     const ticketing = CONSTANTS.OBJECT_STORE["ext.os_cmd.ticketing"];
@@ -15,17 +15,22 @@ exports.os_cmd = cmd => {
     CONSTANTS.LOGINFO(`[REQUEST]: ${cmd}`);
 
     return new Promise((resolve, reject) => ticketing.getTicket(_=>{
-        exec(cmd, (error, data, stderr) => {
+        let osProcess = exec(cmd, {maxBuffer: CONSTANTS.MAX_STDIO_BUFFER}, (error, data, stderr) => {
             ticketing.releaseTicket();
             
             CONSTANTS.LOGEXEC(`[PID:${process.pid}] ${cmd}`);
 
-            if (data) CONSTANTS.LOGINFO(`[PID:${process.pid}] ${data}`);
+            if (data && !stream) CONSTANTS.LOGINFO(`[PID:${process.pid}] ${data}`);
             if (stderr && error) CONSTANTS.LOGERROR(`[PID:${process.pid}] ${stderr}`);
-            else if (stderr) CONSTANTS.LOGWARN(`[PID:${process.pid}] ${stderr}`);
+            else if (stderr && !stream) CONSTANTS.LOGWARN(`[PID:${process.pid}] ${stderr}`);
 
             if (error) reject(error);
             else resolve(data);
         });
+
+        if (stream) {
+            osProcess.stdout.on("data", data => CONSTANTS.LOGINFO(`[PID:${process.pid}] ${data}`));
+            osProcess.stderr.on("data", data => CONSTANTS.LOGWARN(`[PID:${process.pid}] ${data}`));
+        }
     }));
 }
---
+++ extensions/proc_compiler.js
@@ -0,0 +1,37 @@
+/* 
+ * (C) 2019 TekMonks. All rights reserved.
+ * License: MIT - see enclosed LICENSE file.
+ * 
+ * Oracle Pro*C Compiler Class
+ */
+
+const path = require("path");
+const {os_cmd} = require(`${CONSTANTS.EXTDIR}/os_cmd.js`);
+
+ exports.proc_compile = (source_files, compiler_cmd, include_paths, output_directory, output_extension, dblogin) => {
+    if (!output_extension) output_extension = ".o";
+
+    const includeOption = "include=";
+    const outputOption = "oname=";
+    const inputOption = "iname=";
+    const dbOption = "userid=";
+
+    let includeOptions = "";
+    for (const include_path of include_paths) includeOptions += `${includeOption}"${include_path}" `;
+
+    let osCmds = [];
+    for (let source_file of source_files) {
+        source_file = path.resolve(source_file);
+
+        let output_file = path.basename(source_file);
+        output_file = path.resolve(`${output_directory}/${output_file.substring(0, output_file.lastIndexOf("."))}${output_extension}`);
+        
+        const cmd = `${compiler_cmd} ${includeOptions} ${dblogin?dbOption+`"${dblogin}"`:""} ${inputOption}"${source_file}" ${outputOption}"${output_file}"`;
+        osCmds.push(cmd);
+    }
+
+    let promises = [];
+    for (cmd of osCmds) promises.push(os_cmd(cmd));
+
+    return Promise.all(promises);
+ }
