8845bfd9f31607031081c37600abe2fadacc0c84
TekMonksGitHub <rvkapoor@tekmonks.com>
2021-09-20 21:36:39 +0900

    Add in sample for generating cacheable files.

---
+++ samples/genCacheableFilesList.xf.js
@@ -0,0 +1,27 @@
+/*
+ * XForge Build file to generate a list of cacheable files for web applications
+ */
+const path = require("path");
+const fspromises = require("fs").promises;
+
+// build
+exports.make = async function(webroot, out, filterPattern) {
+	try {
+        if ((!webroot)||(!out)) throw "Bad incoming arguments."; // check usage
+
+        // normalize paths
+        webroot = path.resolve(webroot).split(path.sep).join(path.posix.sep); out = path.resolve(out);
+        
+        // find all static files and add to the list
+        const allFiles = (await CONSTANTS.SHELL.ls("-Rl",webroot));
+
+        const cacheableFiles = (filterPattern ? allFiles.filter(fileStat=>fileStat.isFile() && (!fileStat.name.match(filterPattern))) : allFiles).map(fileStat => fileStat.name)
+        await fspromises.writeFile(out, JSON.stringify(cacheableFiles, null, 4));
+
+		CONSTANTS.LOGSUCCESS();
+	} catch (err) { 
+        CONSTANTS.LOGHELP("Build command format: xforge -c -f genCacheableFilesList.xf.js -o webroot -o outfile [-o filterPattern]");
+        CONSTANTS.LOGHELP("Webroot is the webroot, outfile is the JSON file to write to, and filter pattern, if provided, skips files with names matching the pattern.");
+		return CONSTANTS.HANDLE_BUILD_ERROR(err); 
+	}
+}
